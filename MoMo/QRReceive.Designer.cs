1a428cfa1804c5ba12cce2382aee287cabef1af0
daotrananhtuan09102002 <79463278+daotrananhtuan09102002@users.noreply.github.com>
2023-06-14 00:12:10 +0700

    NewUI

---
+++ MoMo/QRReceive.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace MoMo
+{
+    partial class QRReceive
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QRReceive));
+            panel2 = new Panel();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            label1 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            panel1 = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            panel4 = new Panel();
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            label2 = new Label();
+            panel5 = new Panel();
+            label7 = new Label();
+            pictureBox3 = new PictureBox();
+            label6 = new Label();
+            pictureBox2 = new PictureBox();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Fuchsia;
+            panel2.Controls.Add(iconButton2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(iconButton1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(417, 75);
+            panel2.TabIndex = 8;
+            // 
+            // iconButton2
+            // 
+            iconButton2.BackColor = Color.Fuchsia;
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.CircleInfo;
+            iconButton2.IconColor = Color.White;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.Location = new Point(358, 8);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(65, 62);
+            iconButton2.TabIndex = 2;
+            iconButton2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(103, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(221, 46);
+            label1.TabIndex = 1;
+            label1.Text = "QR Nhận tiền";
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.Fuchsia;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.ArrowLeft;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(6, 6);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(65, 62);
+            iconButton1.TabIndex = 0;
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(11, 145);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(395, 530);
+            panel1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(37, 433);
+            label5.Name = "label5";
+            label5.Size = new Size(103, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Thêm lời nhắn";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(37, 342);
+            label4.Name = "label4";
+            label4.Size = new Size(130, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Thêm số tiền nhận";
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.BackgroundImageLayout = ImageLayout.Stretch;
+            panel3.Controls.Add(textBox2);
+            panel3.Location = new Point(13, 445);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(367, 61);
+            panel3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(6, 13);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Chuyển tiền cho mình nhé";
+            textBox2.Size = new Size(339, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
+            panel4.BackgroundImageLayout = ImageLayout.Stretch;
+            panel4.Controls.Add(textBox1);
+            panel4.Location = new Point(13, 354);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(367, 61);
+            panel4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(6, 13);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "0đ";
+            textBox1.Size = new Size(349, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(81, 98);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(232, 232);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(141, 56);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 25);
+            label3.TabIndex = 1;
+            label3.Text = "0373488359";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(92, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(210, 31);
+            label2.TabIndex = 0;
+            label2.Text = "Đào Trần Anh Tuấn";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Transparent;
+            panel5.BackgroundImage = (Image)resources.GetObject("panel5.BackgroundImage");
+            panel5.BackgroundImageLayout = ImageLayout.Stretch;
+            panel5.Controls.Add(label7);
+            panel5.Controls.Add(pictureBox3);
+            panel5.Controls.Add(label6);
+            panel5.Controls.Add(pictureBox2);
+            panel5.Location = new Point(11, 763);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(394, 61);
+            panel5.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(291, 18);
+            label7.Name = "label7";
+            label7.Size = new Size(89, 25);
+            label7.TabIndex = 4;
+            label7.Text = "Tải xuống";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(232, 3);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(53, 57);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(71, 18);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 25);
+            label6.TabIndex = 2;
+            label6.Text = "Tải lại QR";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 3);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(53, 57);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // QRReceive
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 894);
+            Controls.Add(panel5);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "QRReceive";
+            Text = "QRReceive";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private Label label1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label2;
+        private Label label5;
+        private Label label4;
+        private Panel panel3;
+        private TextBox textBox2;
+        private Panel panel4;
+        private TextBox textBox1;
+        private Panel panel5;
+        private Label label7;
+        private PictureBox pictureBox3;
+        private Label label6;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ MoMo/TransferMoneyDetail.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace MoMo
+{
+    partial class TransferMoneyDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransferMoneyDetail));
+            panel2 = new Panel();
+            label1 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel1 = new Panel();
+            label5 = new Label();
+            panel4 = new Panel();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            panel3 = new Panel();
+            textBox2 = new TextBox();
+            panel10 = new Panel();
+            pictureBox5 = new PictureBox();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel10.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Fuchsia;
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(iconButton1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(417, 75);
+            panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(111, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Trần Phú Vinh";
+            // 
+            // iconButton1
+            // 
+            iconButton1.BackColor = Color.Fuchsia;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.ChevronLeft;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(3, 8);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(46, 56);
+            iconButton1.TabIndex = 0;
+            iconButton1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(55, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(48, 43);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(111, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(123, 28);
+            label2.TabIndex = 4;
+            label2.Text = "0373488359";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BackgroundImage = (Image)resources.GetObject("panel1.BackgroundImage");
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(panel4);
+            panel1.Location = new Point(7, 129);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(402, 192);
+            panel1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(43, 103);
+            label5.Name = "label5";
+            label5.Size = new Size(117, 20);
+            label5.TabIndex = 1;
+            label5.Text = "Lời nhắn (0/160)";
+            // 
+            // panel4
+            // 
+            panel4.BackgroundImage = (Image)resources.GetObject("panel4.BackgroundImage");
+            panel4.BackgroundImageLayout = ImageLayout.Stretch;
+            panel4.Controls.Add(textBox1);
+            panel4.Location = new Point(19, 114);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(367, 61);
+            panel4.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(13, 14);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Nhập lời nhắn";
+            textBox1.Size = new Size(339, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(43, 7);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Số tiền chuyển";
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.BackgroundImageLayout = ImageLayout.Stretch;
+            panel3.Controls.Add(textBox2);
+            panel3.Location = new Point(19, 18);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(367, 61);
+            panel3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(13, 14);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "0đ";
+            textBox2.Size = new Size(339, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            panel10.BackColor = Color.White;
+            panel10.Controls.Add(pictureBox5);
+            panel10.Dock = DockStyle.Bottom;
+            panel10.Location = new Point(0, 812);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(417, 82);
+            panel10.TabIndex = 13;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(8, 8);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(403, 69);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 5;
+            pictureBox5.TabStop = false;
+            // 
+            // TransferMoneyDetail
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 894);
+            Controls.Add(panel10);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "TransferMoneyDetail";
+            Text = "TransferMoneyDetail";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel10.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private Label label1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Label label3;
+        private Panel panel3;
+        private TextBox textBox2;
+        private Label label5;
+        private Panel panel4;
+        private TextBox textBox1;
+        private Panel panel10;
+        private PictureBox pictureBox5;
+    }
+}
