8e828b51fdcdaccfc13950c754a835c48b82ff08
Zrmikstri <tpvinhhcm@gmail.com>
2023-06-14 15:06:54 +0700

    fix contact item avatar image

--- MoMo/MyUserControl/ContactItem.Designer.cs
+++ MoMo/MyUserControl/ContactItem.Designer.cs
@@ -41,6 +41,7 @@
             pictureBox1.Location = new Point(3, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(50, 50);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
